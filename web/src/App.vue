a0ce91016f0053b3d2ffba1b7c3ffacfe38860a3
delong zhu <yalishizhude@163.com>
2017-09-08 17:54:31 +0800

    recover web

---
+++ web/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    router-view
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="stylus">
+#app {
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+}
+</style>
---
+++ web/src/vue-shimes.d.ts
@@ -0,0 +1,9 @@
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
+
+declare module 'service' {
+  import axios from 'axios'
+  export default axios
+}
